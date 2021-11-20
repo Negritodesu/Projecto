8d93da9488789f1ce6e8364ed53be7d0754ba542
Negritodesu <bastixxd10@gmail.com>
2021-11-20 12:15:22 -0300

    Avance sistema de cuentas
    Sin terminar

---
+++ Blog/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html',
+                                                authentication_form=UserLoginForm), name = 'login'),
+
+    path('profile/', views.profile, name='profile')
+    
+]
