5762535b9542d203fbaa895c4ff03d278a3377aa
Negritodesu <bastixxd10@gmail.com>
2021-12-08 01:05:02 -0300

    Update urls.py

--- Blog/core/urls.py
+++ Blog/core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace = 'blog')),
+    path('', include('Blog.blog.urls', namespace = 'blog')),
     path('account/', include('accounts.urls', namespace='accounts')),
     path('account/', include('django.contrib.auth.urls')),
     path('services/', include('services.urls', namespace='services')),
