e4223f1e2138830b9c15457567862611fc058e2a
Negritodesu <bastixxd10@gmail.com>
2021-12-08 11:55:23 -0300

    Create urls.py

---
+++ Blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.home, name = 'homepage'),
+    path('cuestionario/', views.cuestionario, name = 'cuestionario'),
+    path('<slug:post>/', views.post_single, name='post_single'),
+    path('consejos/<int:id_consejos>/', views.consejo, name = 'consejos')
+]
+#arhrh
